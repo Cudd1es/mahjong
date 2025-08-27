dd50790bb01e91f7ebb25dd1f60f9adddbc19f32
Cudd1es <j.ghost@icloud.com>
2025-08-27 15:07:13 -0400

    refactor the hand_checker.py, split melds into pungs and chows for yaku

--- hand_checker.py
+++ hand_checker.py
@@ -31,11 +31,10 @@ def is_win_hand(hand:list):
         return True, "chiitoitsu"
 
     # check standard
-    for i in range(len(hand) - 1):
-        if hand[i] == hand[i + 1]:
-            remaining = hand[:i] + hand[i + 2:]
-            if can_form_melds(remaining):
-                return True, "standard"
+    is_standard_winning, head, pungs, chows = try_split_standard_hand(hand)
+    if is_standard_winning:
+        return True, "standard"
+
     return False
 
 def is_kokushi(hand:list[Tile]):
@@ -64,6 +63,69 @@ def is_chiitoitsu(hand:list[Tile]):
         return True
     return False
 
+def try_split_standard_hand(hand:list[Tile]):
+    """
+    split hand into head and melds
+    """
+    hand = [normalize_red(tile) for tile in hand]
+    hand = sort_hand(hand)
+    for i in range(len(hand) - 1):
+        if hand[i] == hand[i + 1]:
+            head = [hand[i] + hand[i + 1]]
+            remaining = hand[:i] + hand[i + 2:]
+            flag, pungs, chows = split_melds(remaining)
+            if flag:
+                return True, head, pungs, chows
+    return False, None, [], []
+
+
+def split_melds(tiles, pung_list=None, chow_lsit=None):
+    """
+    split hand into melds
+    if no remaining tile in the list after split, it is considered as a winning hand
+    """
+    if pung_list is None:
+        pung_list = []
+    if chow_lsit is None:
+        chow_lsit = []
+
+    if not tiles:
+        return True, pung_list[:], chow_lsit[:] # no remaining tile in the list means the original tiles is consisted of melds
+    if len(tiles) % 3 != 0:
+        return False, [], []
+
+    first_tile = tiles[0]
+
+    # check pung
+    if tiles.count(first_tile) >= 3:
+        new_tiles = tiles[:]
+        meld = []
+        for _ in range(3):
+            new_tiles.remove(first_tile)
+            meld.append(first_tile)
+        flag, pungs, chows = split_melds(new_tiles, pung_list + [meld], chow_lsit)
+        if flag:
+            return True, pungs, chows
+
+    # check chow
+    if first_tile.suit in ("m", "p", "s"):
+        val = first_tile.value
+        if val <= 7:
+            second_tile = Tile(first_tile.suit, val+1)
+            third_tile = Tile(first_tile.suit, val+2)
+            if second_tile in tiles and third_tile in tiles:
+                new_tiles = tiles[:]
+                meld = [first_tile, second_tile, third_tile]
+                new_tiles.remove(first_tile)
+                new_tiles.remove(second_tile)
+                new_tiles.remove(third_tile)
+                flag, pungs, chows = split_melds(new_tiles, pung_list, chow_lsit + [meld])
+                if flag:
+                    return True, pungs, chows
+    return False, [], []
+
+
+# no longer used
 def can_form_melds(tiles:list):
     """
     check if the tiles is consisted of melds
@@ -75,7 +137,7 @@ def can_form_melds(tiles:list):
 
     first_tile = tiles[0]
 
-    # check Triplet
+    # check pung
     if tiles.count(first_tile) >= 3:
         new_tiles = tiles[:]
         for _ in range(3):
